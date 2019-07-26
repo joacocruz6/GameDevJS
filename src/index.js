8d7766d1728d84e03af2cbefc3c2082e2e5f147e
Joaquin Cruz D <joacocruz6@gmail.com>
2019-07-26 18:08:21 -0400

    Add the refreshing of the canvas

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 let canvas = document.querySelector("#gameScreen");
 let ctx = canvas.getContext("2d"); // Rendering context to drawing on the canvas
+ctx.clearRect(0,0,800,600); // It's to refresh the canvas because it's keeping the previous figures on there
 ctx.fillStyle="#f00"; // We change the fill of the rectangle to red
 ctx.fillRect(20,20, 100,100) // Draw a rectangule on the canvas, the firm is xpos,ypos,width,height
 ctx.fillRect(300,300,40,40); // This is also a red rectangle
