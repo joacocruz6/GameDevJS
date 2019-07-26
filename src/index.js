4e83e0d472555d21938955085ce6304e4c909a8b
Joaquin Cruz D <joacocruz6@gmail.com>
2019-07-26 18:25:15 -0400

    Add paddel to the game

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
+import Paddle from './paddle';
+// I'm on the 10:42
 let canvas = document.querySelector("#gameScreen");
 let ctx = canvas.getContext("2d"); // Rendering context to drawing on the canvas
-ctx.clearRect(0,0,800,600); // It's to refresh the canvas because it's keeping the previous figures on there
-ctx.fillStyle="#f00"; // We change the fill of the rectangle to red
-ctx.fillRect(20,20, 100,100) // Draw a rectangule on the canvas, the firm is xpos,ypos,width,height
-ctx.fillRect(300,300,40,40); // This is also a red rectangle
- // To change the color we change the style
- ctx.fillStyle="#00f"; // Blue color now
- ctx.fillRect(400,400,40,40);
+
+const GAME_WIDTH = 800;
+const GAME_HEIGHT = 600;
+ctx.clearRect(0,0,800,600);
+
+
+let paddle = new Paddle(GAME_WIDTH,GAME_HEIGHT);
+paddle.draw(ctx);
